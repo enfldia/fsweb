938d486ae3c854f712927fcbae82a1b54894423a
enfldia <enfldia12@naver.com>
2023-09-12 18:05:00 +0900

    0912

---
+++ 72_slide/슬라이드 데이터.js
@@ -0,0 +1,32 @@
+
+        // 데이터 정의
+        const imgObj ={
+            imgData1: {
+                    imgSrc: 'images/pic1.jpg',
+                    imgAlt: '버거1'
+            },
+            imgData2: {
+                    imgSrc: 'images/pic2.jpg',
+                    imgAlt: '버거2'
+            },
+            imgData3: {
+                    imgSrc: 'images/pic3.jpg',
+                    imgAlt: '버거3'
+            },
+            imgData4: {
+                    imgSrc: 'images/pic4.jpg',
+                    imgAlt: '버거4'
+            },
+            imgData5: {
+                    imgSrc: 'images/pic5.jpg',
+                    imgAlt: '버거5'
+            }/* ,
+            imgData6: {
+                    imgSrc: 'images/pic6.jpg',
+                    imgAlt: '버거6'
+            },
+            imgData7: {
+                    imgSrc: 'images/pic7.jpg',
+                    imgAlt: '버거7'
+            } */
+        }

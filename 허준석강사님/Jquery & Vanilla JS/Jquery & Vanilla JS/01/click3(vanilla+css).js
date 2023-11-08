4edf3512ba069218013d406ed27369eed30967a2
enfldia <enfldia12@naver.com>
2023-11-09 02:04:06 +0900

    1109

--- 허준석강사님/Jquery & Vanilla JS/Jquery & Vanilla JS/01/click3(vanilla+css).js
+++ 허준석강사님/Jquery & Vanilla JS/Jquery & Vanilla JS/01/click3(vanilla+css).js
@@ -1,13 +1,12 @@
-;
-var gnbLiA = document.querySelectorAll('.gnb>li>a');
-for (var _i = 0, gnbLiA_1 = gnbLiA; _i < gnbLiA_1.length; _i++) {
-    var item = gnbLiA_1[_i];
+var btn = document.querySelectorAll('.gnb>li>a');
+for (i = 0; i < btn.length; i++) {
+    var item = btn[i];
     item.addEventListener('click', function () {
-        var bbb = this.nextElementSibling.classList.contains('on');
-        if (!bbb) {
+        var open = this.nextElementSibling.classList.contains('on');
+        if (!open) {
             var allSub = document.querySelectorAll('.sub');
-            for (var _i = 0, allSub_1 = allSub; _i < allSub_1.length; _i++) {
-                var item_1 = allSub_1[_i];
+            for (i = 0; i < allSub.length; i++) {
+                var item_1 = allSub[i];
                 item_1.classList.remove('on');
             }
             this.nextElementSibling.classList.add('on');
